6151d0e1e82e8e4749d0490e4ced026739452ecf
Klythu <131404827+Klythu@users.noreply.github.com>
2024-04-17 22:26:15 +0300

    HW2

---
+++ HW2/HW2.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	input := "чоаодбодоржэ"
+	fmt.Println("String is ", input)
+	leters := make(map[rune]int)
+	for _, char := range input {
+		leters[char]++
+	}
+	for char, fric := range leters {
+		ffric := (float32(fric) / float32(len(input))) * 200
+		leter := string(char)
+		fmt.Printf("%s - %.2f \n", leter, ffric)
+	}
+}
